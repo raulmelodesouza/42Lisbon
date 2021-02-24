3892f9b250ea98fa18c56499c90197c9df7b0e4f
Raul Melo de souza <rmelo-de@c1r18s1.42lisboa.com>
2021-02-24 13:53:33 +0000

    Memcpy

--- 42Cursus/libft/Testers/libft/ft_memcpy.c
+++ 42Cursus/libft/Testers/libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rmelo-de <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 14:52:05 by rmelo-de          #+#    #+#             */
-/*   Updated: 2021/02/16 13:33:49 by rmelo-de         ###   ########.fr       */
+/*   Updated: 2021/02/24 13:52:23 by rmelo-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_memcpy(void *s1, const void *s2, size_t n)
 {
 	size_t i;
 
-	if (s1 == 0)
-		return (NULL);
 	if (s1 == s2)
 		return (s1);
 	if (s1 == NULL && s2 == NULL)
