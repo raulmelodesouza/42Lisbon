4ec10e83f183d2945483282255b2ae9814d0012d
Raul Melo de souza <rmelo-de@c1r18s1.42lisboa.com>
2021-03-09 13:00:21 +0000

    printf subject

---
+++ 42Cursus/printf/printfDoc.c
@@ -0,0 +1,18 @@
+********************************************************* 42Cursus *********************************************************
+
+
+------------------------------------Fourth Project - ft_printf-----------------------------------
+
+Sources:
+https://medium.com/@SergioPietri/what-are-variadic-functions-in-c-6205e26c729f
+https://medium.com/@zhang.yine/ft-printf-d95747b7aa5a
+https://github.com/supalarry/ft_printf
+https://www.lix.polytechnique.fr/~liberti/public/computing/prog/c/C/FUNCTIONS/format.html
+https://github.com/agavrel/42-ft_printf
+https://linux.die.net/man/3/printf
+https://github.com/yuki-katayama/42cursus-ft_printf
+
+vim modifications: vim ~/.vimrc
+
+
+X------------------CONCEITOS IMPORTANTES PARA A IMPLEMENTACAO A FUNCAO------------------X
