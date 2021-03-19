86e6ce3f374f67f233df7da574240f418f946406
Raul Melo de souza <rmelo-de@c1r18s1.42lisboa.com>
2021-03-19 14:20:40 +0000

    pointer fix

--- 42Cursus/printf/Testers/ft_printf/ft_printpointer.c
+++ 42Cursus/printf/Testers/ft_printf/ft_printpointer.c
@@ -12,6 +12,19 @@
 
 #include "libftprintf.h"
 
+static int	ft_number_len_long(unsigned long int number, int base_size)
+{
+	size_t	len;
+
+	len = 1;
+	while (number / base_size > 0)
+	{
+		len++;
+		number /= base_size;
+	}
+	return (len);
+}
+
 void		ft_printpointer(va_list ap, t_flags *flags)
 {
 	unsigned long int	address;
@@ -20,7 +33,7 @@ void		ft_printpointer(va_list ap, t_flags *flags)
 	int					precision;
 
 	address = va_arg(ap, unsigned long int);
-	len = ft_numberlen(address, 16);
+	len = ft_number_len_long(address, 16);
 	precision = flags->precision > len ? flags->precision - len : 0;
 	len = !address && flags->dot && !flags->precision ? 2 : len + 2;
 	space = flags->width > len ? flags->width - len : 0;
@@ -36,4 +49,5 @@ void		ft_printpointer(va_list ap, t_flags *flags)
 	if (flags->minus)
 		while (space-- > 0)
 			ft_putchar_fd(' ', 1);
+
 }
